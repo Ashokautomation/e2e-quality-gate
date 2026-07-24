1f99451f3a4c94be4925c9fe46180dec3f015a59
Ashokautomation <ashokenv@gmail.com.com>
2026-07-24 11:44:04 +0530

    feat: use real test data for 3 users in cross-browser matrix

---
+++ data/ecom-users.ts
@@ -0,0 +1,20 @@
+export const ecomUserDataset: EcomUser[] = [
+  {
+    username: 'ashok.test1@gmail.com', // Your real test account
+    password: 'Test123!',              // Your real password
+    productCategory: 'Skincare',
+    productName: 'Absolute Anti-Aging Spot Treatment'
+  },
+  {
+    username: 'ashok.test2@gmail.com', 
+    password: 'Test456!',              
+    productCategory: 'Men',
+    productName: 'Men - Ultra Protection SPF50'
+  },
+  {
+    username: 'ashok.test3@gmail.com', 
+    password: 'Test789!',              
+    productCategory: 'Hair Care',
+    productName: 'Pantene Pro-V Smooth & Sleek'
+  }
+];
